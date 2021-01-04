c39128c964cc208b97dcf0dffc23e411d6c64ab8
dly <1083404373@qq.com>
2021-01-04 23:12:08 +0800

    结构

---
+++ ch2/Sales_data.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Dooooooooo21 on 2021/1/4.
+//
+
+#include "Sales_data.h"
+
+int main() {
+    Sales_data data1, data2;
+}
---
+++ ch2/Sales_data.h
@@ -0,0 +1,18 @@
+//
+// Created by Dooooooooo21 on 2021/1/4.
+//
+
+#ifndef C___LEARN_SALES_DATA_H
+#define C___LEARN_SALES_DATA_H
+
+
+#include <string>
+
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+};
+
+
+#endif //C___LEARN_SALES_DATA_H
