acd70e650ba06a392fd368414bb284bbeffbe727
dly <1083404373@qq.com>
2021-01-05 14:40:04 +0800

    引用和指针

--- ch2/Sales_data.cpp
+++ ch2/Sales_data.cpp
@@ -9,6 +9,7 @@ using namespace std;
 
 namespace A {
     int x = 1;
+
     void fun() {
         cout << "A" << endl;
     }
@@ -16,6 +17,7 @@ namespace A {
 
 namespace B {
     int x = 2;
+
     void fun() {
         cout << "B" << endl;
     }
@@ -27,6 +29,18 @@ namespace B {
 
 using namespace B;
 
+int getMax(const int *arr, int count, bool isMax) {
+    int tmp = arr[0];
+
+    for (int i = 1; i < count; i++) {
+        if ((arr[i] > tmp and isMax) or (arr[i] < tmp and !isMax)) {
+            tmp = arr[i];
+        }
+    }
+
+    return tmp;
+}
+
 int main() {
     Sales_data data1, data2;
     data1.bookNo = "1001";
@@ -36,4 +50,27 @@ int main() {
     cout << A::x << endl;
     B::fun();
     fun2();
+
+    int arr[4] = {1, 3, 5, 6};
+    int res = getMax(arr, 4, true);
+    cout << res << endl;
+
+    // 引用
+    int a;
+    int &b = a;
+    cout << a << endl;
+    cout << b << endl;
+    b = 10;
+    cout << a << endl;
+    cout << b << endl;
+
+    // 指针
+    int aa = 111;
+    int *bb = &aa;
+    cout << aa << endl;
+    cout << *bb << endl;
+    *bb = 222;
+    cout << aa << endl;
+    cout << *bb << endl;
+
 }
