2115d28c8646a6dc0deec4dbf13536d5201dcb5a
dly <1083404373@qq.com>
2021-01-05 12:29:15 +0800

    命名空间

--- ch2/Sales_data.cpp
+++ ch2/Sales_data.cpp
@@ -1,9 +1,39 @@
 //
 // Created by Dooooooooo21 on 2021/1/4.
 //
-
+#include<iostream>
 #include "Sales_data.h"
+
+// 命名空间
+using namespace std;
+
+namespace A {
+    int x = 1;
+    void fun() {
+        cout << "A" << endl;
+    }
+}
+
+namespace B {
+    int x = 2;
+    void fun() {
+        cout << "B" << endl;
+    }
+
+    void fun2() {
+        cout << "2B" << endl;
+    }
+}
+
+using namespace B;
 
 int main() {
     Sales_data data1, data2;
+    data1.bookNo = "1001";
+    cout << "sales" << endl;
+    cout << data1.bookNo << endl;
+
+    cout << A::x << endl;
+    B::fun();
+    fun2();
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-int main() {
-    std::cout << "enter two nums: " << std::endl;
-    int v1 = 0, v2 = 0;
-    std::cin >> v1 >> v2;
-    std::cout << "sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
-    return 0;
-}
+//int main() {
+//    std::cout << "enter two nums: " << std::endl;
+//    int v1 = 0, v2 = 0;
+//    std::cin >> v1 >> v2;
+//    std::cout << "sum of " << v1 << " and " << v2 << " is " << v1 + v2 << std::endl;
+//    return 0;
+//}
