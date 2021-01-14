8de8facab2d6a23b2ed7cb66588d2f4fde3cfd58
dly <1083404373@qq.com>
2021-01-14 13:31:38 +0800

    常量

---
+++ cainiao/daily.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Dooooooooo21 on 2021/1/14.
+//
+
+#include <iostream>
+
+using namespace std;
+
+#define LENGTH 10
+#define WIDTH 5
+#define NEWLINE '\n'
+
+int main() {
+
+    const int l = 10;
+    const int w = 5;
+    const char newline = '\n';
+
+    cout << LENGTH * WIDTH;
+    cout << NEWLINE;
+
+    return 0;
+}
